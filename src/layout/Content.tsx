6d9807795559af2dec28b712e194677661a5bd82
Jeon Sangwan <maxswjeon@naver.com>
2020-03-15 00:12:25 +0900

    Add Background Color for Content

--- src/layout/Content.tsx
+++ src/layout/Content.tsx
@@ -10,12 +10,19 @@ interface ContentProps {
 export default class Content extends Component<ContentProps> {
     render(): React.ReactElement {
         return (
-            <Container>
-                <ContentHeader>PYTHON</ContentHeader>
-            </Container>
+            <Background>
+                <Container>
+                    <ContentHeader>PYTHON</ContentHeader>
+                    <div/>
+                </Container>
+            </Background>
         );
     }
 };
+
+const Background = styled.div`
+  background-color: #F9F9F9;
+`;
 
 const Container = styled.div`
   margin-top: -50px;
